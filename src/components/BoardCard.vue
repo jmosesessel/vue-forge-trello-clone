10a9933586f3acb97eff5ed163e7cb79caca47f3
Joseph Essel Moses <jmosesessel21@gmail.com>
2022-07-13 02:39:15 +0000

    added actions-build.yml file

---
+++ src/components/BoardCard.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="">
+        <template>
+            <div slot="image">
+                
+            </div>
+        </template>
+        
+        
+    </div>
+</template>
